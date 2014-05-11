ef55f5ceb09332956eb98252a89494c674e707d9
Austin Herring <awh44@drexel.edu>
2014-05-11 01:18:42 -0400

    Can extract table metadata
    Added the ability to extract the name of the table from the xml file, as well as the fields for the table and their data types (s, for string, or i, for integer). Currently these values are just (cryptically) printed to stdout.

--- database.c
+++ database.c
@@ -3,6 +3,9 @@
 #include <fcntl.h>
 #include <string.h>
 
+#define NAME_START 14
+#define BUFFER_SIZE 64
+
 typedef struct
 {
 	char *name;
@@ -17,6 +20,7 @@ typedef struct
 typedef struct
 {
 	char *name;
+	int num_fields;
 	Field *fields;
 	Row *rows;
 } Table;
@@ -29,17 +33,15 @@ typedef struct
 } Database;
 
 void read_row(Field *field, FILE *file);
+char *get_field_value(char **field, const char *start, const char *delim);
 void read_table(Table *table, const char *table_def, FILE *file);
 int read_database(Database *database, const char *db_name);
 
 int main(int argc, char *argv[])
 {
 	Database database;
-	database.name = NULL;
-	database.num_tables = 0;
-	database.tables = NULL;
-
 	read_database(&database, "test_db.xml");
+
 	return 0;
 }
 
@@ -66,6 +68,8 @@ int read_database(Database *database, const char *db_name)
 	//database values
 	database->name = (char *) malloc((strlen(db_name) + 1) * sizeof(char));
 	strcpy(database->name, db_name);
+	database->num_tables = 0;
+	database->tables = NULL;
 
 	chars_read = getline(&line, &line_size, db_file);
 	while (strcmp(line, "</database>\n") != 0)
@@ -80,9 +84,42 @@ int read_database(Database *database, const char *db_name)
 	fclose(db_file);
 }
 
+char *get_field_value(char **field, const char *start, const char *delim)
+{
+	char *end = strstr(start, delim);
+	*end = '\0';
+	int length = strlen(start);
+	*field = (char *) malloc((length + 1) * sizeof(char));
+	strcpy(*field, start);
+	*end = delim[0];
+	return end + 1;
+}
+
 void read_table(Table *table, const char *table_def, FILE *file)
 {
-	
+	//initialize table
+	table->num_fields = 0;
+	table->fields = NULL;
+	table->rows = NULL;
+
+	char *next_start = get_field_value(&table->name, &table_def[NAME_START], "\"");
+	while (*next_start != '>')
+	{
+		table->num_fields++;
+		table->fields = realloc(table->fields, table->num_fields * sizeof(Field));
+		next_start = get_field_value(&table->fields[table->num_fields - 1].name, next_start + 1, "=");
+		printf("%s\n", table->fields[table->num_fields - 1].name);
+		next_start = get_field_value(&table->fields[table->num_fields - 1].type, next_start + 1, "\"");
+		printf("%s\n", table->fields[table->num_fields - 1].type);
+	}
+
+	char *line = NULL;
+	size_t line_size = 0;
+	int chars_read = getline(&line, &line_size, file);
+	while (strcmp(line, "\t</table>\n") != 0)
+	{
+		getline(&line, &line_size, file);
+	}
 }
 
 void read_row(Field *field, FILE *file)
